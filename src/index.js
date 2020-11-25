075da5d99fa88f5e99ebf013063c644a8a054b23
kobayashi-kenta <72721542+kobayashi-kenta@users.noreply.github.com>
2020-11-26 00:12:11 +0900

    完成

---
+++ src/index.js
@@ -0,0 +1,67 @@
+const onClickAdd = () => {
+  const inputText = document.getElementById("add-text").value;
+  document.getElementById("add-text").value = "";
+
+  createIncompletelist(inputText);
+};
+
+//未完成リストから指定の要素を削除
+const deleteFromUndoList = (target) => {
+  document.getElementById("undo-list").removeChild(target);
+};
+
+const createIncompletelist = (text) => {
+  //divの生成
+  const div = document.createElement("div");
+  div.className = "list-row";
+
+  //liタグ生成
+  const li = document.createElement("li");
+  li.innerText = text;
+
+  //button(完了）タグ生成
+  const completeButton = document.createElement("button");
+  completeButton.innerText = "完了";
+  completeButton.addEventListener("click", () => {
+    deleteFromUndoList(deleteButton.parentNode);
+    const addTarget = completeButton.parentNode;
+    const text = addTarget.firstElementChild.innerText;
+
+    addTarget.textContent = null;
+    const li = document.createElement("li");
+    li.innerText = text;
+
+    const backButton = document.createElement("button");
+    backButton.innerText = "戻す";
+    backButton.addEventListener("click", () => {
+      const deleteTarget = backButton.parentNode;
+      document.getElementById("done-list").removeChild(deleteTarget);
+
+      const text = backButton.parentElement.firstElementChild.innerText;
+      createIncompletelist(text);
+    });
+
+    addTarget.appendChild(li);
+    addTarget.appendChild(backButton);
+
+    document.getElementById("done-list").appendChild(addTarget);
+  });
+
+  //button(削除）タグ生成
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "削除";
+  deleteButton.addEventListener("click", () => {
+    deleteFromUndoList(deleteButton.parentNode);
+  });
+
+  //divタグの子要素に各要素を設定
+  div.appendChild(li);
+  div.appendChild(completeButton);
+  div.appendChild(deleteButton);
+
+  //未完了のリストに追加
+  document.getElementById("undo-list").appendChild(div);
+};
+document
+  .getElementById("add-button")
+  .addEventListener("click", () => onClickAdd());
